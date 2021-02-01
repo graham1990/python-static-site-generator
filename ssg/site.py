63fdf29855251196e93ecb9bf8bb59785c9bdbaa
Graham Waterfield <you@example.com>
2021-02-01 16:10:15 +0000

    wip

--- ssg/site.py
+++ ssg/site.py
@@ -6,12 +6,12 @@ class Site:
     self.dest = Path(dest)
 
   def create_dir(self, path):
-    directory = self.dest + '/' + path.relative_to(self.source)
+    directory = self.dest / path.relative_to(self.source)
     directory.mkdir(parents=True, exist_ok = True)
 
   def build(self):
     self.dest.mkdir(parents=True, exist_ok = True)
-    for each_path in self.source.rglob("*"):
-      if each_path.isDirectory():
-        self.create_dir()
+    for path in self.source.rglob("*"):
+      if path.is_dir():
+        self.create_dir(path)
     
