a75db4695a299d70aab317acd236ba8f3fa3de2a
Graham Waterfield <you@example.com>
2021-02-01 15:43:55 +0000

    wip

---
+++ ssg/site.py
@@ -0,0 +1,9 @@
+import Path from pathlib
+
+class Site:
+  def __init__(self, source, dest):
+    self.source = Path(source)
+    self.dest = Path(dest)
+
+  def create_dir(self, path):
+    directory = self.dest + '/' + path.relative_to(self.source)
